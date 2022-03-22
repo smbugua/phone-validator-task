1844db3a9d3c94797aa6742dd826d251e759af3e
Simon Mbugua <simon.mbugua@andela.com>
2022-03-23 02:27:28 +0300

    update the db engine

--- terraform/infra/rds.tf
+++ terraform/infra/rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "jumiadb" {
   instance_class         = "db.t3.micro"
   allocated_storage      = 10
   engine                 = "postgres"
-  engine_version         = "13.2"
+  engine_version         = "13.3"
   username               = "postgres"
   password               = var.db_password
   db_subnet_group_name   = var.db_subnet_name

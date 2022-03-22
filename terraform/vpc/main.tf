6706c463d72481034e36a6facb00ba57ae197a63
Simon Mbugua <simon.mbugua@andela.com>
2022-03-22 17:49:09 +0300

    security setup and ig atatchment

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region=var.region
 }
+
+#provision vpc
 resource "aws_vpc" "prod-vpc"  {
     source = "terraform-aws-modules/vpc/aws"
     version = "~>3.0"
@@ -19,6 +21,7 @@ resource "aws_vpc" "prod-vpc"  {
     create_database_subnet_route_table = true
 }
 
+#provision subnets
 resource "aws_subnet" "public-subnet-1" {
   vpc_id = "${aws_vpc.prod-vpc.id}"  
   cidr_block = "10.10.0.0/24"
@@ -91,3 +94,81 @@ resource "aws_subnet" "private-subnet-3" {
   }
 
 }
+
+
+#provision ig
+
+resource "aws_internet_gateway" "prod-igw" {
+    vpc_id = "${aws_vpc.prod-vpc.id}"
+    tags {
+        Name = "prod-igw"
+    }
+}
+
+
+
+#provision route table
+resource "aws_route_table" "prod-public-crt" {
+    vpc_id = "${aws_vpc.main-vpc.id}"
+    
+    route {
+        //associated subnet can reach everywhere
+        cidr_block = "0.0.0.0/0" 
+        //CRT uses this IGW to reach internet
+        gateway_id = "${aws_internet_gateway.prod-igw.id}" 
+    }
+    
+    tags {
+        Name = "prod-public-crt"
+    }
+}
+
+#assouciate my route table with public subnets
+resource "aws_route_table_association" "prod-crta-public-subnet-1"{
+    subnet_id = "${aws_subnet.public-subnet-1.id}"
+    route_table_id = "${aws_route_table.prod-public-crt.id}"
+}
+
+
+resource "aws_route_table_association" "prod-crta-public-subnet-2"{
+    subnet_id = "${aws_subnet.public-subnet-2.id}"
+    route_table_id = "${aws_route_table.prod-public-crt.id}"
+}
+
+
+resource "aws_route_table_association" "prod-crta-public-subnet-3"{
+    subnet_id = "${aws_subnet.public-subnet-2.id}"
+    route_table_id = "${aws_route_table.prod-public-crt.id}"
+}
+
+#security group 
+resource "aws_security_group" "ssh-allowed" {
+    vpc_id = "${aws_vpc.prod-vpc.id}"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        // This means, all ip address are allowed to ssh ! 
+        // Do not do it in the production. 
+        // Put your office or home address in it!
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    //If you do not add this rule, you can not reach the NGIX  
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags {
+        Name = "ssh-allowed"
+    }
+}
+

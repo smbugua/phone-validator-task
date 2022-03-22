4680ac83f77a8d72338887b5c7d51e3ca5b0f85d
Simon Mbugua <simon.mbugua@andela.com>
2022-03-23 01:35:41 +0300

    fix errors

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -36,24 +36,6 @@ module "vpc" {
 }
 
 
-#provision route table
-resource "aws_route_table" "prod-public-crt" {
-    vpc_id = module.vpc.vpc_id
-    
-    route {
-        //associated subnet can reach everywhere
-        cidr_block = "0.0.0.0/0" 
-        //CRT uses this IGW to reach internet
-        gateway_id = "${aws_internet_gateway.prod-igw.id}" 
-    }
-    
-    tags = {
-        Name = "prod-public-crt"
-    }
-
-}
-
-
 #security group 
 resource "aws_security_group" "main_security_group" {
     vpc_id = module.vpc.vpc_id
